d7cc6011e2bee32c0c841233e325c53c79b3efd5
Dheeraj Kumar Bandela <dheeru@Dheerajs-MacBook-Air.local>
2020-02-13 01:41:09 +0530

    changed log dir

--- src/config.py
+++ src/config.py
@@ -7,7 +7,8 @@ DB_PORT = os.environ.get("DB_PORT", 27017)
 WEB3_PROVIDER = os.environ.get("WEB3_PROVIDER", "http://3.219.79.88:8545")
 BASE_BLOCK = os.environ.get("BASE_BLOCK", 4348700)
 BLOCK_OFFSET = os.environ.get("BLOCK_OFFSET", 2)
-LOG_FILE = "info_{}_{}.log".format(BASE_BLOCK, BASE_BLOCK+BLOCK_OFFSET)
+LOG_FILE = "/usr/log/info_{}_{}.log".format(
+    BASE_BLOCK, BASE_BLOCK+BLOCK_OFFSET)
 DB_NAME = os.environ.get("DB_NAME", "eth_db")
 BLOCK_COLLECTION = os.environ.get("BLOCK_COLLECTION", "blocks")
 TRANSACTION_COLLECTION = os.environ.get(
