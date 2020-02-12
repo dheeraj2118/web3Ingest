e5292bbed414c913bd8b08171d5d70749aca5509
Dheeraj Kumar Bandela <dheeru@Dheerajs-MacBook-Air.local>
2020-02-12 23:47:47 +0530

    config fix

--- src/config.py
+++ src/config.py
@@ -2,13 +2,14 @@ import os
 
 # Config information
 
-DB_HOST = os.environ["DB_HOST"] | | "52.201.108.190"
-DB_PORT = os.environ["DB_PORT"] | | "27017"
-WEB3_PROVIDER = os.environ["WEB3_PROVIDER"] | | "http://3.219.79.88:8545"
-BASE_BLOCK = os.environ["BASE_BLOCK"] | | 1
-BLOCK_OFFSET = os.environ["BLOCK_OFFSET"] | | 10000
+DB_HOST = os.environ.get("DB_HOST", "52.201.108.190")
+DB_PORT = os.environ.get("DB_PORT", "27017")
+WEB3_PROVIDER = os.environ.get("WEB3_PROVIDER", "http://3.219.79.88:8545")
+BASE_BLOCK = os.environ.get("BASE_BLOCK", 1)
+BLOCK_OFFSET = os.environ.get("BLOCK_OFFSET", 10000)
 LOG_FILE = "log/blocks_{}_{}.log".format(
     BASE_BLOCK, BASE_BLOCK+BLOCK_OFFSET-1)
-DB_NAME = os.environ["DB_NAME"] | | "eth_db"
-BLOCK_COLLECTION = os.environ["BLOCK_COLLECTION"] | | "blocks"
-TRANSACTION_COLLECTION = os.environ["TRANSACTION_COLLECTION"] | | "transactions"
+DB_NAME = os.environ.get("DB_NAME", "eth_db")
+BLOCK_COLLECTION = os.environ.get("BLOCK_COLLECTION", "blocks")
+TRANSACTION_COLLECTION = os.environ.get(
+    "TRANSACTION_COLLECTION", "transactions")
