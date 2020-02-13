d8ef4d4afd239fc56eff07e668bef5d5c1f671f7
Ubuntu <ubuntu@ip-172-31-4-6.ec2.internal>
2020-02-13 04:31:54 +0000

    Fixed indentaion for return

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import os
 
 DB_HOST = os.environ.get("DB_HOST", "52.201.108.190")
 DB_PORT = os.environ.get("DB_PORT", 27017)
-WEB3_PROVIDER = os.environ.get("WEB3_PROVIDER", "http://3.219.79.88:8545")
+WEB3_PROVIDER = os.environ.get("WEB3_PROVIDER", "http://127.0.0.1:8545")
 BASE_BLOCK = int(os.environ.get("BASE_BLOCK", 4348700))
 BLOCK_OFFSET = int(os.environ.get("BLOCK_OFFSET", 2))
 LOG_FILE = "/usr/log/info_{}_{}.log".format(
--- src/ingest_blocks.py
+++ src/ingest_blocks.py
@@ -51,51 +51,52 @@ def get_transaction_info(w3, transaction_hash):
 
 
 def load_blocks(base_block, block_offset):
+    print (base_block)
+    print (block_offset)
     w3 = get_web3_connection(WEB3_PROVIDER)
     db = get_db_connection(DB_HOST, DB_PORT, DB_NAME)
     blocks = db[BLOCK_COLLECTION]
     transactions = db[TRANSACTION_COLLECTION]
     for block_id in range(base_block, base_block+block_offset+1):
+        print(block_id)
         block = get_block_dict(w3, block_id)
         if block:
             block["totalDifficulty"] = str(block["totalDifficulty"])
             block["difficulty"] = str(block["difficulty"])
             try:
                 blocks.insert_one(block)
+                logging.info("success: {}".format(block_id))
+                print("success: {}".format(block_id))
             except Exception as e:
-                logging.info(
-                    "{} Block Insertion Failed {}".format(block_id, e))
+                print("{} Block Insertion Failed {}".format(block_id, e))
+                logging.info("{} Block Insertion Failed {}".format(block_id, e))
             for transaction in block["transactions"]:
                 transaction_id = transaction.hex()
-                transaction_details = get_transaction_info(
-                    w3, transaction.hex())
+                transaction_details = get_transaction_info(w3, transaction.hex())
                 if transaction_details:
-                    transaction_details["value_wei"] = str(
-                        transaction_details["value"])
-                    transaction_details["value"] = transaction_details["value"] / \
-                        pow(10, 18)
-                    transaction_details["gas_wei"] = str(
-                        transaction_details["gas"])
-                    transaction_details["gas"] = transaction_details["gas"] / \
-                        pow(10, 18)
-                    transaction_details["gasPrice_wei"] = str(
-                        transaction_details["gasPrice"])
-                    transaction_details["gasPrice"] = transaction_details["gasPrice"] / \
-                        pow(10, 18)
+                    transaction_details["value_wei"] = str(transaction_details["value"])
+                    transaction_details["value"] = transaction_details["value"] / pow(10, 18)
+                    transaction_details["gas_wei"] = str(transaction_details["gas"])
+                    transaction_details["gas"] = transaction_details["gas"] / pow(10, 18)
+                    transaction_details["gasPrice_wei"] = str(transaction_details["gasPrice"])
+                    transaction_details["gasPrice"] = transaction_details["gasPrice"] / pow(10, 18)
                     try:
                         transactions.insert_one(transaction_details)
+                        print("success: {}/{}".format(block_id, transaction_id))
+                        logging.info("success: {}/{}".format(block_id, transaction_id))
                     except Exception as e:
-                        logging.info(
-                            "{} Transaction Insertion Failed {}".format(transaction_id, e))
+                        print("{} Transaction Insertion Failed {}".format(transaction_id, e))
+                        logging.info("{} Transaction Insertion Failed {}".format(transaction_id, e))
 
                 else:
-                    logging.info("Transaction {}  {}".format(
-                        transaction_id, "Failed"))
+                    print("Transaction {}  {}".format(transaction_id, "Failed"))
+                    logging.info("Transaction {}  {}".format(transaction_id, "Failed"))
                     continue
         else:
+            print("Block {} {}".format(block_id, "Failed"))
             logging.info("Block {} {}".format(block_id, "Failed"))
             continue
-        return
+    return
 
 
 load_blocks(BASE_BLOCK, BLOCK_OFFSET)
