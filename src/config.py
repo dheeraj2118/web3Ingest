6eec8d504872e0c2dc15b2017db0b463a48d8fee
Dheeraj Bandela <dheerajkumar.bandela@gmail.com>
2020-02-19 11:41:20 +0530

    set default base and offset block to 0

--- src/config.py
+++ src/config.py
@@ -5,11 +5,11 @@ import os
 DB_HOST = os.environ.get("DB_HOST", "52.201.108.190")
 DB_PORT = os.environ.get("DB_PORT", 27017)
 WEB3_PROVIDER = os.environ.get("WEB3_PROVIDER", "http://3.219.79.88:8545")
-BASE_BLOCK = int(os.environ.get("BASE_BLOCK", 4348700))
-BLOCK_OFFSET = int(os.environ.get("BLOCK_OFFSET", 2))
-LOG_FILE = "/usr/log/info_{}_{}.log".format(
+BASE_BLOCK = int(os.environ.get("BASE_BLOCK", 0))
+BLOCK_OFFSET = int(os.environ.get("BLOCK_OFFSET", 0))
+LOG_FILE = "info_{}_{}.log".format(
     BASE_BLOCK, BASE_BLOCK+BLOCK_OFFSET)
-DB_NAME = os.environ.get("DB_NAME", "eth_db")
+DB_NAME = os.environ.get("DB_NAME", "eth_db_test")
 BLOCK_COLLECTION = os.environ.get("BLOCK_COLLECTION", "blocks")
 TRANSACTION_COLLECTION = os.environ.get(
     "TRANSACTION_COLLECTION", "transactions")
