2b8c8bc833d63b7bcbe0d97cebcebc7a18675b00
Dheeraj Bandela <dheerajkumar.bandela@gmail.com>
2020-03-05 09:09:34 +0530

    Updated to actual DB

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ BASE_BLOCK = int(os.environ.get("BASE_BLOCK", 0))
 BLOCK_OFFSET = int(os.environ.get("BLOCK_OFFSET", 0))
 LOG_FILE = "info_{}_{}.log".format(
     BASE_BLOCK, BASE_BLOCK+BLOCK_OFFSET)
-DB_NAME = os.environ.get("DB_NAME", "eth_db_test")
+DB_NAME = os.environ.get("DB_NAME", "eth_db")
 BLOCK_COLLECTION = os.environ.get("BLOCK_COLLECTION", "blocks")
 TRANSACTION_COLLECTION = os.environ.get(
     "TRANSACTION_COLLECTION", "transactions")
