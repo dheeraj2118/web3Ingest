c18183a4dd8f9b1b6ca4c8e08d8cbdbb3534a3a9
Dheeraj Bandela <dheerajkumar.bandela@gmail.com>
2020-03-05 09:05:56 +0530

    Updated host IP address

--- src/config.py
+++ src/config.py
@@ -2,9 +2,9 @@ import os
 
 # Config information
 
-DB_HOST = os.environ.get("DB_HOST", "52.201.108.190")
+DB_HOST = os.environ.get("DB_HOST", "18.211.113.218")
 DB_PORT = os.environ.get("DB_PORT", 27017)
-WEB3_PROVIDER = os.environ.get("WEB3_PROVIDER", "http://3.219.79.88:8545")
+WEB3_PROVIDER = os.environ.get("WEB3_PROVIDER", "http://52.21.97.126:8545")
 BASE_BLOCK = int(os.environ.get("BASE_BLOCK", 0))
 BLOCK_OFFSET = int(os.environ.get("BLOCK_OFFSET", 0))
 LOG_FILE = "info_{}_{}.log".format(
